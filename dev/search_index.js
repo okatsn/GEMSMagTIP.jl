d283f5c882b9218fa7dd366893d7513ba4856a48
Documenter.jl <documenter@juliadocs.github.io>
2025-02-21 03:19:33 +0000

    build based on d98e022

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GEMSMagTIP","category":"page"},{"location":"#GEMSMagTIP","page":"Home","title":"GEMSMagTIP","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GEMSMagTIP.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GEMSMagTIP]","category":"page"}]
+}
