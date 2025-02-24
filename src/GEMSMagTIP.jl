26f241cdffd2b7cb9d5996f80e6088fa5495f441
okatsn <okatsn@gmail.com>
2025-02-24 16:00:20 +0800

    Add `BestModels` struct.

--- src/GEMSMagTIP.jl
+++ src/GEMSMagTIP.jl
@@ -13,10 +13,10 @@ end
 
 Phase(v::Vector) = Phase(v...)
 
-function Dates.format(p::Phase)
+function Dates.format(p::Phase, fmt)
     return [
-        Dates.format(p.t0, info_date_format),
-        Dates.format(p.t1, info_date_format)
+        Dates.format(p.t0, fmt),
+        Dates.format(p.t1, fmt)
     ]
 end
 
@@ -72,16 +72,46 @@ end
 
 # JSON serialization
 function Serde.SerJson.ser_type(::Type{Info}, var::Vector{Phase})
-    return [Dates.format(p) for p in var]
+    return [Dates.format(p, info_date_format) for p in var]
     # `p` is a `Phase`
 end
 
 # Serialize a single Phase to date strings
 function Serde.SerJson.ser_type(::Type{Info}, var::Phase)
-    return [Dates.format(var)]
+    return [Dates.format(var, info_date_format)]
 end
 
 
+struct BestModels
+    Mc::Int64
+    Rc::Int64
+    NthrRatio::Float64
+    Tthr::Int64
+    Tobs::Int64
+    Tpred::Int64
+    Tlead::Int64
+    Athr::Int64
+    Lat::Float64
+    Lon::Float64
+    prp::String
+    frc::Phase
+    stn::String
+    AlarmedRate::Float64
+    MissedRate::Float64
+    FittingDegree::Float64
+end
+
+const frc_date_format = "yyyymmdd"
+
+function Serde.deser(::Type{BestModels}, ::Type{Phase}, data)
+    v = split(data, "-")
+    return Phase(Dates.Date.(v, frc_date_format))
+    # `v` is a vector of two date strings.
+end
+
+function Serde.SerJson.ser_type(::Type{BestModels}, var::Phase)
+    return [Dates.format(var, frc_date_format)]
+end
 
 
 end
