9b15124f336dedcf0e7a18d079b06a5b94220678
okatsn <okatsn@gmail.com>
2025-02-25 09:32:56 +0800

    Docstring for constant file names

--- src/const_filename.jl
+++ src/const_filename.jl
@@ -1,3 +1,20 @@
+docstr_file_const = """
+This file name is shared with [GEMS-MagTIP-insider](https://github.com/CGRG-lab/GEMS-MagTIP-insider) data processing.
+
+If this constant has been modified, in matlab scripts which use `statind_summary.m` and `jointstation_summary.m` the output file names have also be modified.
+"""
+
+"""
+$docstr_file_const
+"""
 const file_fittingdegree = Symbol("fittingDegree.csv")
+
+"""
+$docstr_file_const
+"""
 const file_bestmodels = Symbol("bestModels.csv")
+
+"""
+$docstr_file_const
+"""
 const file_statind = Symbol("StatInd.csv")
