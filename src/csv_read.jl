fc81b400ea74c432de8a494861300edb7248bf80
okatsn <okatsn@gmail.com>
2025-02-25 12:03:03 +0800

    CHECKPOINT

--- src/csv_read.jl
+++ src/csv_read.jl
@@ -1,7 +1,7 @@
 function core_read(::Val{file_fittingdegree}, path)
+    # # CHECKPOINT: The following processing works, but superfluous because the final destination of `deser_csv` is `to_deser`, which already accepts rows.
     data0 = CSV.File(path)
     rows = data0 |> CSV.rowtable
-    # # KEYNOTE: The following processing works, but superfluous because the final destination of `deser_csv` is `to_deser`, which already accepts rows.
     # Parse to a string of csv data
     csv0 = rows |> to_csv
     # `deser_csv` calls `parse_csv`, which parse the csv string to NamedTuples, and send the NamedTuples to `to_deser`.
