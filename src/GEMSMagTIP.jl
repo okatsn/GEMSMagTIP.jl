7108331c7580d6f694d6246dc8794a58ab3ab6fc
okatsn <okatsn@gmail.com>
2025-02-25 08:26:38 +0800

    `Phase` constructor for `data::String` input; Refactor `errorshow`.

--- src/GEMSMagTIP.jl
+++ src/GEMSMagTIP.jl
@@ -11,7 +11,15 @@ struct Phase
     t1::Date
 end
 
+# For v a vector of `Date`
 Phase(v::Vector) = Phase(v...)
+
+const frc_date_format = "yyyymmdd"
+
+function Phase(data::String)
+    v = split(data, "-")
+    return Phase(Dates.Date.(v, frc_date_format))
+end
 
 function Dates.format(p::Phase, fmt)
     return [
@@ -32,6 +40,11 @@ How to use: `throw(NotSupported("CSV serialization"))`.
 How to test: `@test_throws GEMSMagTIP.NotSupported ...`
 """
 NotSupported(feature::String) = NotSupported(feature, "Feature '$feature' is not supported yet.")
+
+function Base.showerror(io::IO, e::NotSupported)
+    print(io, "NotSupported: $(e.message)")
+end
+
 
 include("deser_info.jl")
 
--- src/deser_bestmodel.jl
+++ src/deser_bestmodel.jl
@@ -17,11 +17,9 @@ struct BestModels
     FittingDegree::Float64
 end
 
-const frc_date_format = "yyyymmdd"
 
-function Serde.deser(::Type{BestModels}, ::Type{Phase}, data)
-    v = split(data, "-")
-    return Phase(Dates.Date.(v, frc_date_format))
+function Serde.deser(::Type{BestModels}, ::Type{Phase}, data::String)
+    return Phase(data)
     # `v` is a vector of two date strings.
 end
 
@@ -34,11 +32,6 @@ end
 # function Serde.ser_value(::Type{BestModels}, ::Val{:frc}, var::Phase)
 #     return Dates.format(var.t0, frc_date_format) * "-" * Dates.format(var.t1, frc_date_format)
 # end
-
-function Base.showerror(io::IO, e::NotSupported)
-    print(io, "NotSupported: $(e.message)")
-end
-
 function Serde.to_csv(::Vector{BestModels})
     throw(NotSupported("CSV serialization"))
 end
