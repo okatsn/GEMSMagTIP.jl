8c643bc76547253e1700f4a861fab8dbc600ff9f
okatsn <okatsn@gmail.com>
2025-05-15 09:18:01 +0800

    The issue of dictionaries immutability solved.

--- src/freqbands.jl
+++ src/freqbands.jl
@@ -1,4 +1,4 @@
-freq_bands = Dict(
+freq_bands = Base.ImmutableDict(
     "ULF_A" => (0.001, 0.003),
     "ULF_B" => (0.001, 0.01),
     "ULF_C" => (0.001, 0.1),
@@ -6,7 +6,7 @@ freq_bands = Dict(
     "BP_40" => (0.00010, 0.0178),
 )
 
-alternative_names = Dict(
+alternative_names = Base.ImmutableDict(
     "ULF-A" => freq_bands["ULF_A"],
     "ULF-B" => freq_bands["ULF_B"],
     "ULF-C" => freq_bands["ULF_C"],
@@ -14,7 +14,8 @@ alternative_names = Dict(
     "BP-40" => freq_bands["BP_40"],
 )
 
-const freq_bands_ext = merge(freq_bands, alternative_names)
+# KEYNOTE: noted that merge two immutable dictionaries returns a mutable dictionary
+const freq_bands_ext = Base.ImmutableDict([(k => v) for (k, v) in merge(freq_bands, alternative_names)]...)
 
 
 """
--- test/freqbands.jl
+++ test/freqbands.jl
@@ -2,6 +2,7 @@
     @test GEMSMagTIP.FreqBandDict()["BP-35"] == GEMSMagTIP.FreqBandDict()["BP_35"] == (0.00032, 0.0178)
     @test GEMSMagTIP.FreqBandDict()["ULF-B"] == GEMSMagTIP.FreqBandDict()["ULF_B"] == (0.001, 0.01)
 
-    GEMSMagTIP.freq_bands["ULF_B"] = GEMSMagTIP.freq_bands_ext["ULF_B"] = (999, 999)
+    @test_throws MethodError GEMSMagTIP.freq_bands["ULF_B"] = (999, 999)
+    @test_throws MethodError GEMSMagTIP.freq_bands_ext["ULF_B"] = (999, 999)
     @test GEMSMagTIP.FreqBandDict()["ULF-B"] == GEMSMagTIP.FreqBandDict()["ULF_B"] == (0.001, 0.01)
 end
